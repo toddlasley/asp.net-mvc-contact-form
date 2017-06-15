b9cadf3b3f41ccbd771b4c63dce5dcd5ad761cd6
Todd <todd.r.lasley@gmail.com>
2017-06-15 19:40:25 -0400

    added ContactController and Contact/Index view

---
+++ ContactForm/ContactForm/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactForm.Controllers
+{
+    public class ContactController : Controller
+    {
+        /// <summary>
+        /// Returns contact form page.
+        /// </summary>
+        /// <returns></returns>
+        // GET: Contact
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
